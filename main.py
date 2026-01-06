f1de06283f4552b9eb3d970de758ffc4e812cc2f
Katarina Cvetkovic <katarinacv@elfak.rs>
2026-01-06 16:24:42 +0100

    print text fix

--- main.py
+++ main.py
@@ -39,7 +39,7 @@ def main():
                 cipher_text = input("Enter cipher text to be decrypted: ")
 
                 if (len(cipher_text) % 4) != 0:
-                    print("Invalid cipher text length. It must be a multiple of 4.")
+                    print("Invalid cipher text length. It must be a divisible by 4.")
                     continue
 
                 normalized = hc.normalize_text(cipher_text)
